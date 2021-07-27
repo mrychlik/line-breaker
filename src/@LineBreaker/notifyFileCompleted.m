9679ef276946f6fe645f22a8eec685d3ae84e2ca
Marek Rychlik <rychlik@gmail.com>
2021-07-27 02:12:16 -0700

    *** empty log message ***

--- src/@LineBreaker/notifyFileCompleted.m
+++ src/@LineBreaker/notifyFileCompleted.m
@@ -1,6 +1,6 @@
 function this = notifyFileCompleted(this, filename_or_msg)
     if ~isempty(this.app)
-        this.app.FileCompletedLabel.Text = filename_or_msg;
+        this.app.BatchProgressInfo.Text = filename_or_msg;
     else
         fprintf(filename_or_msg);
     end
