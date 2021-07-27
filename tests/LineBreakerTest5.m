84752ed823917c3a823f5de29a3328acb6f20b98
Marek Rychlik <rychlik@gmail.com>
2021-07-27 13:47:05 -0700

    *** empty log message ***

--- tests/LineBreakerTest5.m
+++ tests/LineBreakerTest5.m
@@ -10,7 +10,7 @@
 % Test of batch processing of a single file. Tests 'Force' option
 preamble;
 inputFile = fullfile('..','images','sinat-074.png');
-outputDir = fullfile('.','Lines-sinat-074');
+outputDir = fullfile('..','Bingo','Lines-sinat-074');
 ob = LineBreaker('Force',true,'Verbose',true);
 ob.SmallSizeThreshold = 10;
 ob.batch(inputFile,outputDir);
